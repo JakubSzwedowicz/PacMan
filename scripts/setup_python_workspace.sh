069a6a0dcd799df230b81270227acf668de5d96d
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2025-03-24 10:01:41 +0100

    Update script and diagram on macos.

--- scripts/setup_python_workspace.sh
+++ scripts/setup_python_workspace.sh
@@ -4,5 +4,11 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
+# On mac
+#brew install graphviz
+
 # Verify if it works...
 hpp2plantuml -i "src/**/*.h" -o "Docs/output.puml"
+
+# You can also extract dependencies from methods with -d
+#hpp2plantuml -i "src/**/*.h" -d -o "Docs/output.puml"
