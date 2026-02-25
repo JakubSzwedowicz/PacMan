59411bd9da52695f51c0c46507f0b929819a5cc2
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2026-02-26 00:54:28 +0100

    Add initial Maps implementation in Core.

---
+++ src/core/maps/MapsManager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "core/maps/Map.hpp"
+
+#include <Utils/Logging/Logger.h>
+
+#include <expected>
+#include <filesystem>
+#include <string>
+#include <string_view>
+
+namespace pacman::core::maps {
+
+class MapsManager {
+public:
+  static std::expected<Map, std::string>
+  loadFromFile(const std::filesystem::path &path);
+
+  static std::expected<Map, std::string> loadFromJson(std::string_view json);
+
+private:
+  static std::expected<void, std::string> validate(const Map &map);
+
+  static inline Utils::Logging::Logger m_logger{"MapsManager"};
+};
+
+} // namespace pacman::core::maps
